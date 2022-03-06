68a75b9a302822a8e39e4e6d0de92eeddd595ee8
MZainAshraf <99812309+MZainAshraf@users.noreply.github.com>
2022-03-06 22:16:00 +0500

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,99 @@
+let lessons=[
+    {
+      id: 01,
+      title: "Math",
+      location: "Classroom 01",
+      price: 500,
+      spaces: 5,
+      image: "images/maths.png",
+      rating:4,
+    },
+    {
+      id: 02,
+      title: "Physics",
+      location: "Classroom 02",
+      price: 100,
+      spaces: 5,
+      image: "images/physics.png",
+      rating:4,
+
+    },
+    {
+      id: 03,
+      title: "Chemistry",
+      location: "Classroom 03",
+      price: 2500,
+      spaces: 5,
+      image: "images/chemistry.png",
+      rating:4,
+      },
+      {
+        id: 04,
+        title: "World History",
+        location: "Classroom 04",
+        price: 530,
+        spaces: 5,
+        image: "images/history.png",
+        rating:4,
+
+      },
+      {
+        id: 05,
+        title: "Geography",
+        location: "Classroom 05",
+        price: 150,
+        spaces: 5,
+        image: "images/geography.png",
+        rating: 4,
+      },
+      {
+        id: 06,
+        title: "Sociliology",
+        location: "Classroom 06",
+        price: 230,
+        spaces: 5,
+        image: "images/sociology.png",
+        rating:4,
+     
+        },
+        {
+          id: 07,
+          title: "Creative Arts",
+          location: "Classroom 07",
+          price: 780,
+          spaces: 5,
+          image: "images/arts.png",
+          rating:4,
+
+        },
+        {
+          id: 08,
+          title: "Computer Science",
+          location: "Classroom 08",
+          price: 300,
+          spaces: 5,
+          image: "images/computer.png",
+          rating:5,
+  
+        },
+        {
+          id: 09,
+          title: "Business Studies",
+          location: "Classroom 09",
+          price: 270,
+          spaces: 5,
+          image: "images/business.png",
+          rating:4,
+      
+          },
+          {
+            id: 10,
+            title: "Economics",
+            location: "Classroom 10",
+            price: 130,
+            spaces: 5,
+            image: "images/economics.png",
+            rating:4,
+           
+          },
+  ]
